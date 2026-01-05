4276b03203d514a17067d9773a1f9dc93825acf3
Adham Abdelaal <a.ahmad2313@nu.edu.eg>
2026-01-05 17:10:53 +0200

    adjusted phone number

--- src/components/landing/ContactUs.tsx
+++ src/components/landing/ContactUs.tsx
@@ -83,7 +83,7 @@ export default function ContactUs() {
               <div className={styles.contactItem}>
                 <FaWhatsapp className={styles.icon} />
                 <span>
-                  <strong>Phone:</strong> +20 127 019 9110
+                  <strong>Phone:</strong> +20 100 986 3946
                 </span>
               </div>
               <div className={styles.contactItem}>
